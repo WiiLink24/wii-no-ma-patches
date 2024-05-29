fe149d92d6772d56f64d857c1dabe7e9ed34a717
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2024-05-29 17:40:19 -0400

    Fix DCFlushRange address

--- rvl.h
+++ rvl.h
@@ -3,6 +3,6 @@
 #include <util.h>
 
 namespace RVL {
-    LONGCALL void DCFlushRange(u32* address, u32 size) AT(0x802d2120);
+    LONGCALL void DCFlushRange(u32* address, u32 size) AT(0x802d2150);
     LONGCALL void ICInvalidateRange(u32* address, u32 size) AT(0x802d2240);
 }
