29cdc83a3183da6afb7b5e1d88e262dcb350dd19
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2024-07-06 11:30:15 -0400

    fix: Force NTSC for PAL

--- video.cpp
+++ video.cpp
@@ -1,27 +1,9 @@
-#include <util.h>
+#include <asm.h>
 #include <patch.h>
-#include <rvl.h>
-#include <setting.h>
 
-namespace room::Video {
-    void FixPinkBar() {
-        // We can fix the pink bar by forcing the 480i video mode.
-        // This changes the setting for the whole system which may or may not be favourable, but it works.
-        // We must also detect if this is PAL because a video code of 0 can also be NTSC-U mode.
-        if ((*reinterpret_cast<u8*>(0x808ad361) == 0 || *reinterpret_cast<u8*>(0x808ad361) == 5) && sc::GetAreaCode() == 2)
-        {
-            *reinterpret_cast<u8*>(0x808ad361) = 1;
-            RVL::DCFlushRange(reinterpret_cast<u32*>(0x808ad361), 1);
-            RVL::ICInvalidateRange(reinterpret_cast<u32*>(0x808ad361), 1);
-        }
-
-        LONGCALL void VIInit() AT(0x802e33e0);
-
-        // Now initialize the video
-        VIInit();
-    }
-
-    ROOM_DEFINE_PATCH = {
-        Patch::WriteFunctionCall(0x801c0db0, FixPinkBar)
-    };
+namespace room::Video
+{
+ROOM_DEFINE_PATCH = {
+    Patch::WriteASM(0x802e4ba0, 2, ASM_LAMBDA(li r3, 0x0; blr;)),
+    Patch::WriteU32(0x802e3f24, 0x480000c8)};
 }
